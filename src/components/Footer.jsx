ce7959cfb8f6e18f1c68f33ea726e948aeeff202
Malronjv <mj7marlonvivas@gmail.com>
2023-04-26 17:11:54 -0500

    archivos incluidos

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='d-flex justify-content-center mt-5 pt-5'>
+            <footer className=''>
+                <h6 className='text-white'>
+                    styles modified by Copyright © MJ7
+                </h6>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/containers/Game.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../components/Footer';
+
+const Game = () => {
+  return (
+    <div className='d-flex flex-column'>
+      <h1 className='text-center mb-5 pb-5 text-white'>Piedra, Papel o Tigeras</h1>
+      
+      <Footer />
+    </div>
+  )
+}
+
+export default Game;
